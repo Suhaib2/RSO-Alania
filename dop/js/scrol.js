f878eb5280647195d2660608b7a6fdd61923131b
Suhaib <63431177+Suhaib2@users.noreply.github.com>
2021-09-07 09:20:30 +0300

    Delete dop directory

--- dop/js/scrol.js
+++
@@ -1,6 +0,0 @@
-let progress = document.getElementById('progresbar');
-let totalHeight = document.body.scrollHeight - window.innerHeight;
-window.onscroll = function () {
-	let progressHeight = (window.pageYOffset / totalHeight) * 100;
-	progress.style.height = progressHeight + "%";
-}
